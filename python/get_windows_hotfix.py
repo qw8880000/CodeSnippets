d6795be17e10f019b64a6ef4047915dcf3abafb3
qw8880000 <qw8880000@126.com>
2019-07-15 11:15:43 +0800

    update

--- python/get_windows_hotfix.py
+++ python/get_windows_hotfix.py
@@ -1,24 +1,12 @@
-#!/usr/bin/python
 # -*- coding:cp936 -*-
 
 import sys
-import time
-import logging
 import subprocess
 import json
 
-
-time_str = time.strftime("%Y%m%d_%H%M%S", time.localtime())
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s %(levelname)s %(filename)s %(lineno)d %(message)s',
-    filename='%s.log' % time_str
-)
-
-
 def fetch(kbids=[]):
     output = subprocess.check_output('wmic qfe get hotfixid, installedon')
-    logging.debug(output)
+    # logging.debug(output)
 
     result = []
     output_list = (output.split())[2:]
@@ -26,14 +14,14 @@ def fetch(kbids=[]):
         hotfixid = output_list[i]
         installedon = output_list[i + 1]
         pair = {"hotFixID":hotfixid, "installedOn":installedon}
-        logging.debug(pair)
+        # logging.debug(pair)
 
         if not kbids:
             result.append(pair)
-            logging.debug(result)
+            # logging.debug(result)
         elif kbids and hotfixid in kbids:
             result = pair
-            logging.debug(result)
+            # logging.debug(result)
             break
 
     return json.dumps(result)
@@ -41,6 +29,6 @@ def fetch(kbids=[]):
 
 if __name__ == '__main__':
     kbids = sys.argv[1:] if len(sys.argv) > 0 else []
-    logging.debug(kbids)
+    # logging.debug(kbids)
     result = fetch(kbids)
     print(result)
